54c6351bb1f67cba15e75f45d7b48a44077f0fca
Evan Brink <evanbrink@Evans-MacBook-Pro-4.local>
2019-01-06 17:32:35 -0500

    Temporary Button images added

--- DrinkMachineCore.py
+++ DrinkMachineCore.py
@@ -2,7 +2,7 @@ from guizero import *
 import DrinkSerial
 import time
 
-## ---------------LOAD IN SETTINGS-------------------
+#  ---------------LOAD IN SETTINGS-------------------
 rum         = 1
 vodka       = 2
 tequila     = 3
@@ -20,12 +20,12 @@ tonic       = 12
 
 
 
-## ---------------GLOBAL VARIALBES-------------------
+#  ---------------GLOBAL VARIALBES-------------------
 window_height = 700
 window_width = 1000
 image_size = 250
 
-## Calibration constants
+#  Calibration constants
 cc1 = 1
 cc2 = 0.98
 cc3 = 1.1
@@ -63,7 +63,7 @@ class Recipe:
 
 DarkAndStormy = Recipe(ingredients = [rum, gingerB, lime],
                        proportions = [0.36,0.55,0.09], image =
-                       "DarkAndStormy.jpg", addMessage = "Enjoy!")
+                       "DarkAndStormyButton.png", addMessage = "Enjoy!")
 
 Margarita = Recipe(ingredients = [tequila, lime, tripleS],
                    proportions = [0.57, 0.29, 0.14], image =
@@ -72,39 +72,39 @@ Margarita = Recipe(ingredients = [tequila, lime, tripleS],
 
 Cosmopolitan = Recipe(ingredients = [vodka, lime, tripleS, cranberry],
                       proportions = [0.67, 0.11, 0.11, 0.11], image =
-                      "Cosmopolitan.jpg", addMessage = "Enjoy!")
+                      "CosmopolitanButton.png", addMessage = "Enjoy!")
 
 RumPunch = Recipe(ingredients = [rum, orange, pineapple, cranberry],
                   proportions = [0.31, 0.23, 0.23, 0.23], image =
-                  "RumPunch.jpg", addMessage =
+                  "RumPunchButton.png", addMessage =
                   "Suggested: Add 1 pump Grenadine")
 
 MoscowMule = Recipe(ingredients = [vodka, gingerB, lime],
                     proportions = [0.24, 0.71, 0.06], image =
-                    "MoscowMule.jpg", addMessage = "Enjoy!")
+                    "MoscowMuleButton.png", addMessage = "Enjoy!")
 
 TequilaSunrise = Recipe(ingredients = [tequila, orange],
                         proportions = [0.33, 0.67], image =
-                        "TequilaSunrise.jpg", addMessage =
+                        "TequilaSunriseButton.png", addMessage =
                         "Add 2 pumps Grenadine")
 
 VodkaCranberry = Recipe(ingredients = [vodka, cranberry, lime],
                         proportions = [0.15, 0.77, 0.08], image =
-                        "VodkaCranberry.jpg", addMessage = "Enjoy!")
+                        "VodkaCranberryButton.png", addMessage = "Enjoy!")
 
 SexOnTheBeach = Recipe(ingredients = [vodka, peachS, orange, cranberry],
                        proportions = [0.13, 0.13, 0.37, 0.37], image =
-                       "SexOnTheBeach.jpg", addMessage = "Enjoy!")
+                       "SexOnTheBeachButton.png", addMessage = "Enjoy!")
 
 Mojito = Recipe(ingredients = [rum, lime, clubS], proportions =
-                [0.23, 0.15, 0.62], image = "Mojito.jpg", addMessage =
+                [0.23, 0.15, 0.62], image = "MojitoButton.png", addMessage =
                 "Add 2 pumps simple syrup + 2 pumps mint syrup")
 
-## -------------------GUI SETUP---------------------------
+#  -------------------GUI SETUP---------------------------
 app = App(title="MouthSmash", width=window_width,
          height=window_height, layout="grid")
 
-## Pages
+#  Pages
 page_one = Box(app, grid=[0,0], align="top", layout="grid")
 page_one_text = Text(page_one, grid = [0,0], align = "top", text = "PAGE ONE")
 page_two = Box(app, grid=[0,0], align="top", layout="grid",
@@ -190,8 +190,9 @@ def cmd07():
     print(MoscowMule.ing)
 
 mojitoButton = DrinkButton(cmd01, "MojitoButton.png", page_one, 0,0)
-pinaColadaButton = DrinkButton(cmd02, "Pina-Colada.jpg", page_one, 1,0)
+pinaColadaButton = DrinkButton(cmd02, "PinaColadaButton.png", page_one, 1,0)
 darkAndStormyButton = DrinkButton(cmd03, DarkAndStormy.image, page_one, 2,0)
+cosmopolitanButton = DrinkButton(cmd05, Cosmopolitan.image, page_one, 0,1)
 margaritaButton = DrinkButton(cmd04, Margarita.image, page_two, 0,0)
 #cosmopolitanButton = DrinkButton(cmd05, Cosmopolitan.image, page_two, 1,0)
 rumPunchButton = DrinkButton(cmd06, RumPunch.image, page_two, 2,0)
